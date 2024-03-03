27f3888311d98f85d1f39cc1838b7fa429159d5b
KlazHTT <account@haothitran.com>
2024-03-03 00:07:06 -0500

    Edit comment

--- Core/Localization.lua
+++ Core/Localization.lua
@@ -22,11 +22,11 @@ if locale == 'en_GB' or locale == 'en_US' then return end -- ENGLISH
 
 -- if locale == 'esMX' then return end  -- SPANISH (MEXICO)
 -- if locale == 'pt_BR' then return end -- PORTUGEUSE
--- if locale == 'de_DE' then return end  -- GERMAN
--- if locale == 'es_ES' then return end  -- SPANISH (SPAIN)
--- if locale == 'fr_FR' then return end  -- FRENCH
+-- if locale == 'de_DE' then return end -- GERMAN
+-- if locale == 'es_ES' then return end -- SPANISH (SPAIN)
+-- if locale == 'fr_FR' then return end -- FRENCH
 -- if locale == 'it_IT' then return end -- ITALIAN
--- if locale == 'ru_RU' then return end  -- RUSSIAN
+-- if locale == 'ru_RU' then return end -- RUSSIAN
 -- if locale == 'ko_KR' then return end -- KOREAN
--- if locale == 'zh_TW' then return end  -- CHINESE (TRADITIONAL)
--- if locale == 'zh_CN' then return end  -- CHINESE (SIMPLIFIED)
+-- if locale == 'zh_TW' then return end -- CHINESE (TRADITIONAL)
+-- if locale == 'zh_CN' then return end -- CHINESE (SIMPLIFIED)
